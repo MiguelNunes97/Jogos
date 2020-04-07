1d9d2962a57ed1175db970437224cb3312bcb44c
MiguelNunes97 <63251096+MiguelNunes97@users.noreply.github.com>
2020-04-07 11:21:17 +0100

    Add files via upload

---
+++ movePlayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movePlayer : MonoBehaviour
+{
+    public GameObject cauda;
+    private Rigidbody2D rb1;
+
+    float speed = 1.5f;
+
+    public void Start()
+    {
+        StartCoroutine(tail());
+        rb1 = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+
+        if (Input.GetKey("w"))
+
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        if (Input.GetKey("s"))
+
+            transform.Translate(- Vector3.up * speed * Time.deltaTime);
+        if (Input.GetKey("d"))
+
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        if (Input.GetKey("a"))
+            transform.Translate(- Vector3.right * speed * Time.deltaTime);
+
+    }
+
+
+    IEnumerator tail()
+    {
+        while (true)
+        {
+            Instantiate(cauda, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(0.15f);
+        }
+    }
+}
